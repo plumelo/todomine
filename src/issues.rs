c3e0f31ede0bd92173a6771ffd654ea601711187
Meghea Iulian <iulian.meghea@gmail.com>
2023-05-01 11:25:51 +0300

    feat: initilize download redmine issues to a todo.txt file

--- src/issues.rs
+++ src/issues.rs
@@ -1,13 +1,34 @@
 use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+use todo_txt::Task;
 
-use reqwest;
+use reqwest::{
+    self,
+    header::{ACCEPT, CONTENT_TYPE},
+};
 
-#[derive(Deserialize)]
-struct Issue {
+#[derive(Deserialize, Debug)]
+struct Project {
     id: u16,
+    name: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
+struct Status {
+    id: u16,
+    name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Issue {
+    id: u16,
+    project: Project,
+    status: Status,
+    subject: String,
+    description: String,
+}
+
+#[derive(Deserialize, Debug)]
 pub struct Issues {
     issues: Vec<Issue>,
     total_count: u16,
@@ -20,17 +41,51 @@ pub struct Filter {
     offset: Option<u16>,
     limit: Option<u16>,
     sort: Option<String>,
-
     issue_id: Vec<String>,
-    project_id: String,
+    project_id: Option<String>,
     status_id: String,
-    assign_to: String,
+    assigned_to_id: Option<String>,
+}
+impl Default for Filter {
+    fn default() -> Self {
+        Self {
+            offset: None,
+            limit: None,
+            sort: None,
+            issue_id: vec![],
+            project_id: None,
+            status_id: "*".to_string(),
+            assigned_to_id: None,
+        }
+    }
+}
+
+impl Issue {
+    pub fn into_task(self) -> Task {
+        let mut tags = BTreeMap::new();
+        tags.insert("rid".to_string(), self.id.to_string());
+        Task {
+            subject: self.subject,
+            tags,
+            projects: vec![self.project.name],
+            ..Task::default()
+        }
+    }
+}
+impl Issues {
+    pub fn into_tasks(self) -> Vec<Task> {
+        let mut tasks: Vec<Task> = vec![];
+        for issue in self.issues {
+            tasks.push(issue.into_task());
+        }
+        tasks
+    }
 }
 
 pub struct ListIssues {
     url: String,
     key: String,
-    filter: Option<Filter>,
+    filter: Filter,
 }
 
 impl ListIssues {
@@ -38,20 +93,19 @@ impl ListIssues {
         Self {
             url,
             key,
-            filter: None,
+            filter: Filter::default(),
         }
     }
 
-    pub async fn get(&self) -> reqwest::Result<Issues> {
-        let url = self.url.to_owned() + "issues.json";
+    pub async fn get(self) -> reqwest::Result<Issues> {
+        let url = vec![self.url, "issues.json".to_string()].join("/");
         let client = reqwest::Client::builder().build()?;
         let res = client
             .get(url)
-            .query(&self.filter)
-            .header("X-Redmine-API-Key", &self.key)
+            //.query(&self.filter)
+            .header("X-Redmine-API-Key", self.key)
             .send()
             .await?;
-
         let issues = res.json::<Issues>().await?;
         Ok(issues)
     }
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,13 @@
-use clap::{Args, Parser, Subcommand};
-use create::create;
+use anyhow::Result;
+use clap::Parser;
+use sync::Sync;
 
-mod create;
 mod issues;
-
-#[derive(Parser)]
-#[command(name = "todomine")]
-#[command(author = "Iulian M. <iulian.meghea@gmail.com>")]
-#[command(version = "0.1")]
-#[command(about = "Synchronize Redmine issues with todo.txt files.", long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Create a new todo.txt file from Redmine issues.
-    Create(CreateArgs),
-}
-
-#[derive(Args)]
-struct RedmineArgs {
-    project: String,
-}
-
-#[derive(Args)]
-struct CreateArgs {
-    name: String,
-    url: String,
-    key: String,
-}
+mod sync;
 
 #[tokio::main]
-async fn main() {
-    let cli = Cli::parse();
-
-    match &cli.command {
-        Commands::Create(name) => {
-            println!("'myapp create' was used, name is: {:?}", name.name);
-            create();
-        }
-    }
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    let cli = Sync::parse();
+    cli.sync().await?;
+    Ok(())
 }
