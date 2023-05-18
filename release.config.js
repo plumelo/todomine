84aed3ef578391f1e14dff3460990839eab75fa0
Meghea Iulian <iulian.meghea@gmail.com>
2023-05-18 09:32:20 +0300

    chore(ci): adjust semantic release

--- release.config.js
+++ release.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  release: {
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      "@semantic-release/github",
-      "@semantic-release/git"
-    ],
-    branch: "master",
-    preset: "conventionalcommits"
-  },
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    "@semantic-release/git"
+  ],
+  branch: "master",
+  preset: "conventionalcommits"
 }
